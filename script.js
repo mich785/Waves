ab0c423adc8d77e9d3d16128206525b837211c6b
michelle nderi <nderimichelle@gmail.com>
2023-03-12 23:05:00 +0300

    Add JS event listener

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-
+function logSubmit(event) {
+    log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById("form");
+  const log = document.getElementById("log");
+  form.addEventListener("submit", logSubmit);
   
   
